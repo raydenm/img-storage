8d972fe44ae9fcc49d986a779f3d32c899e1e8e4
raydenm <raydenm928@gmail.com>
2024-08-12 11:04:56 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# 图床
+
+利用Cloudflare Pages部署稳定的无限图床Telegraph，实现图片上传和访问，并进一步通过WordPress的WP.COM全球图片缓存进行加速，提高图片加载速度。可用于免费图片托管解决方案，Flickr/imgur 等替代品。
+
+## 简介
+
+- [Cloudflare Pages](https://pages.cloudflare.com/) 是一个强大的静态网站托管服务，结合了 Cloudflare 的全球 CDN（内容分发网络）优势。
+
+- [Telegraph](https://telegra.ph/) 是一个简单而有效的匿名发布平台，可以用于快速上传和管理内容，特别是图片。
+
+- [WordPress 的全球图片缓存](https://01.wp.com/) 是一个高效的 CDN 服务，专门用于加速 WordPress 托管的图片内容。它利用全球分布的节点，将图片缓存并提供快速访问。
+
+- [Cloudflare CDN（内容分发网络）](https://www.cloudflare.com/zh-cn/application-services/products/cdn/)是由Cloudflare提供的服务，旨在加速和保护和加速全球网络应用程序。
+
+## 如何部署
+
+- 1、准备一个 Cloudflare 账户
+- 2、Fork 本仓库，自由修改`App.vue`和`index.html`文件中的文案
+- 3、登录`Cloudflare Dashboard`打开`Workers 和 Pages`创建`Pages`
+- 4、`连接到Git`选择`Github`或`Gitlab`中你刚刚Fork的项目，点击开始设置
+- 5、只需要修改`框架预设`为`Vue`即可，点击保存并部署，即可部署成功并投入使用
+
+
+### 特点
+
+- 无限图片储存数量，你可以上传不限数量的图片到`Telegraph`
+
+- 无需购买服务器，托管于`Cloudflare Pages`上，每天10万次的请求
+
+- 无需购买域名，可以使用`Cloudflare Pages` 提供的`*.pages.dev`的免费二级域名，同时也支持绑定自定义域名
+
---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <a-watermark class="page_main" content="图床" :alpha="0.1">
+    <header>
+      <h1>Img Storage</h1>
+      <div class="desc">
+        <p>无限图片储存数量，你可以上传不限数量的图片！</p>
+        <p>图片首次访问后缓存，"永久"有效，包括全球分布的 CDN，以确保尽可能快地提供图像.</p>
+        <p>项目使用无服务器服务搭建，托管在 <a-tag color="gray"><template #icon><icon-cloud /></template>Cloudflare Pages</a-tag> 上运行.
+        </p>
+      </div>
+    </header>
+    <main>
+      <a-upload draggable :action="ACTION_API" multiple :show-link="false" :image-preview="true"
+        :show-remove-button="false" accept=".png,.jpg,.jpeg,.gif" :on-before-upload="checkImageSizeFn"
+        @success="scrollToBottom">
+        <template #upload-item="item">
+          <div class="vh-img-item" :key="item.fileItem.uid">
+            <a-image class="vh_img_val" :src="item.fileItem.url" />
+            <div class="vh_img_text" v-if="item.fileItem.status == 'uploading'">
+              <a-skeleton :animation="true">
+                <a-space direction="vertical" :style="{ width: '100%' }" size="mini">
+                  <a-skeleton-line :rows="2" />
+                </a-space>
+              </a-skeleton>
+            </div>
+            <div class="vh_img_text with_value" v-else>
+              <section>
+                <a-input :error="item.fileItem.response == 'Failed'" :default-value="item.fileItem.response" readonly
+                  size="mini" @click="item.fileItem.response != 'Failed' && copyStr(item.fileItem.response)">
+                  <template #append v-if="item.fileItem.response != 'Failed'">URL</template>
+                </a-input>
+                <a-input :error="item.fileItem.response == 'Failed'"
+                  :default-value="`![${item.fileItem.name}](${item.fileItem.response})`" readonly size="mini"
+                  @click="item.fileItem.response != 'Failed' && copyStr(`![${item.fileItem.name}](${item.fileItem.response})`)">
+                  <template #append v-if="item.fileItem.response != 'Failed'">Markdown</template>
+                </a-input>
+              </section>
+              <a-popover title="图片二维码">
+                <qrcode-vue class="qr" :value="item.fileItem.response" :size="56" level="H" />
+                <template #content>
+                  <qrcode-vue class="qr" :value="item.fileItem.response" :size="166" level="H" />
+                </template>
+              </a-popover>
+            </div>
+          </div>
+        </template>
+      </a-upload>
+    </main>
+    <footer>
+      <p><img src="./assets/svg/ing.svg"></p>
+      <p>
+        <a href="https://pages.cloudflare.com" target="_blank" rel="noopener noreferrer"><img
+            src="./assets/svg/framework.svg"></a>
+        <a href="https://wordpress.com" target="_blank" rel="noopener noreferrer"><img src="./assets/svg/cache.svg"></a>
+        <a href="https://www.cloudflare.com/zh-cn/application-services/products/cdn/" target="_blank"
+          rel="noopener noreferrer"><img src="./assets/svg/cdn.svg"></a>
+        <a href="https://vuejs.org" target="_blank" rel="noopener noreferrer"><img src="./assets/svg/web.svg"></a>
+      </p>
+    </footer>
+  </a-watermark>
+</template>
+<script setup lang="ts">
+import { nextTick } from 'vue'
+import { Message } from '@arco-design/web-vue'
+import QrcodeVue from 'qrcode.vue'
+const ACTION_API = import.meta.env.VITE_IMG_API_URL;
+
+
+// 检查图片大小
+const checkImageSizeFn = (file: any) => {
+  const maxSize = 5 * 1024 * 1024;
+  if (file.size > maxSize) {
+    Message.error(`图片大小不能超过 ${maxSize / 1024 / 1024}MB`);
+    return false;
+  }
+  return true;
+}
+
+// 复制返回地址
+const copyStr = async (v: any) => {
+  try {
+    await navigator.clipboard.writeText(v);
+  } catch (error) {
+    const i = document.createElement("textarea");
+    i.value = v;
+    document.body.appendChild(i);
+    i.select();
+    document.body.removeChild(i);
+  }
+  Message.success('Copy Success')
+}
+
+// 上传完毕滚动到最下面
+const scrollToBottom = () => {
+  nextTick(() => {
+    const imgListDom: any = document.querySelector('.arco-upload-list');
+    imgListDom.scrollTo({ top: imgListDom.scrollHeight, behavior: "smooth" });
+  })
+}
+</script>
+<style scoped lang="less">
+@import './assets/less/main.less';
+</style>
